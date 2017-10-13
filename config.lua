26c6aa8820093b44322d44100a04e88ffbfd9c7c
Unknown <charly.strn@gmail.com>
2017-10-13 21:43:41 +0200

    update readme

--- config.lua
+++ config.lua
@@ -1,11 +1,12 @@
 Config                        = {}
-Config.Locale 				  			= 'fr'
-Config.green 				  				= 56108
-Config.grey 				  				= 8421504
-Config.red 					  				= 16711680
-Config.orange 				  			= 16744192
-Config.blue 				  				= 2061822
-Config.purple 								= 11750815
+Config.Locale 				  = 'fr'
+Config.green 				  = 56108
+Config.grey 				  = 8421504
+Config.red 					  = 16711680
+Config.orange 				  = 16744192
+Config.blue 				  = 2061822
+Config.purple 				  = 11750815
+Config.webhook                = "PASTE_HERE_YOUR_WEBHOOK"
 
 
 settings = {
--- server/main.lua
+++ server/main.lua
@@ -4,7 +4,7 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 --Send the message to your discord server
 function sendToDiscord (name,message,color)
-  local DiscordWebHook ="https://discordapp.com/api/webhooks/366299007930073091/b5YKimk1JPlWfGXuH-8AgdkF3kLQR3I3gzb2RYeml2mn01NsLwscFtVY8qJxXnf0y0tt"
+  local DiscordWebHook = Config.webhook
   -- Modify here your discordWebHook username = name, content = message,embeds = embeds
 
 local embeds = {
@@ -122,7 +122,7 @@ end)
 RegisterServerEvent("esx:jackingcar")
 AddEventHandler("esx:jackingcar", function(model)
    local xPlayer = ESX.GetPlayerFromId(source)
-   sendToDiscord(_U('server_carjacking'),xPlayer.name.." "..('user_carjacking').." ".. model,Config.purple)
+   sendToDiscord(_U('server_carjacking'),xPlayer.name.." ".._('user_carjacking').." ".. model,Config.purple)
 
 end)
 
